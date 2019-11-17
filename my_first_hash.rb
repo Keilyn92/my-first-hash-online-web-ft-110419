c869baffe15de412aee2058b9676fe17b003d755
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-17 19:11:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -40,13 +40,13 @@ def adding
   
   # add 2 muskets to the shipping_manifest hash below
        
-              puts shipping_manifest["muskets"] = 2
+              shipping_manifest["muskets"] = 2
           
   # add 4 gun powder to the shipping_manifest hash below
-             puts shipping_manifest["gun powder"] = 4
+             shipping_manifest["gun powder"] = 4
               
 
 
   # return the shipping_manifest hash below
-
+    return shipping_manifest
 end
