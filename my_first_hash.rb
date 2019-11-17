4f19971991beca37bb6c1a1f5df0396c2a8e1493
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-17 18:46:11 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -39,9 +39,7 @@ def adding
                   "oil paintings" => 3}
   
   # add 2 muskets to the shipping_manifest hash below
-        # shipping_manifest["jar of molasses"] = 10
- 
-#puts shipping_manifest
+       
               shipping_manifest["muskets"] = 2
           
   # add 4 gun powder to the shipping_manifest hash below
